1b04ed5d981521073f9864eaea8112cfc8681551
zain abu salah <zainabusalah11@hotmail.com>
2022-11-15 10:05:21 +0200

    init

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-      private FrameLayout loginframe;
+
+      //private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.loginfrag, new loginfragment());
+        ft.commit();
     }
 
 
 
-    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frameLayoutMain, new LoginFragment());
-        ft.commit();
+    public void Welcome(View view) {
+
+    }
 }
